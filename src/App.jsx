f63009695a3b2d19a201cabc2526741c0ce5e3fe
D1nast <almond1024r@gmail.com>
2022-10-06 16:16:16 +0000

    Update App.jsx and ColorfulMessage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ColorfulMessage from "./Components/ColorfulMessage.jsx";
 //{}はjsを記述できる
 //cssに充てる記述は文字列として書く
 //Componentsにcolor等適当な名前を付けてpropsを渡せる
+//props=Componentに渡す引数みたいなもの
 const App = () => {
 const onClickButton=()=>{alert()};
   return (
--- src/Components/ColorfulMessage.jsx
+++ src/Components/ColorfulMessage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 //props.childrenはタグで囲った中身を表示
+//分割代入
 const ColorfulMessage=(props)=>{
+    const {color,children}=props;
     const contentStyle={
-    color:props.color,
+    color:color,
     fontSize:'18px'
 };
     return(
-        <p style={contentStyle}>{props.children}</p>
+        <p style={contentStyle}>{children}</p>
     );
 }
 
