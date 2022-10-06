7476ccbdaf5d894791c58bc1104bf65b0430bac1
D1nast <almond1024r@gmail.com>
2022-10-06 16:27:36 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React,{useState} from "react";
 import ColorfulMessage from "./Components/ColorfulMessage.jsx";
 //{}はjsを記述できる
 //cssに充てる記述は文字列として書く
 //Componentsにcolor等適当な名前を付けてpropsを渡せる
 //props=Componentに渡す引数みたいなもの
+//const [num,setNum]=useState(0); 変数　更新関数　初期値を設定する
 const App = () => {
-const onClickButton=()=>{alert()};
+const onClickCountUp=()=>{
+    setNum(num+1);
+};
+const [num,setNum]=useState(0);
   return (
     <>
     <h1 style={{color:"red"}}>Hello</h1>
     <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
     <ColorfulMessage color="pink">Agate</ColorfulMessage>
-    <button onClick={onClickButton}>ボタン</button>
+    <button onClick={onClickCountUp}>カウントアップ</button>
+    <p>{num}</p>
     </>
   );
 };
